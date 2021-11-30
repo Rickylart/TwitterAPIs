ccc8b32035e9a1da95c9df5774b23aea0bb942a8
RickyLart <larteyfrancis@ymail.com>
2021-11-30 19:54:05 +0000

    Updated APIs

---
+++ routes/twitterAPI/followers.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const fetch = require('node-fetch');
+var express = require('express');
+var router = express.Router();
+const got = require('got');
+
+router.use('/',function(req, res){
+
+    let userID = req.body.userid;
+
+    console.log(userID);
+
+    if(!typeof userID === 'undefined' || userID === null){
+    
+        res.status(400);
+        res.write(JSON.stringify("UserID Can't be Empty"));
+        return  res.end();
+       }
+       else{
+
+        fetch(` https://api.twitter.com/2/users/${userID}/followers`,{
+            method: 'GET',
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization":process.env.BEARER_TOKEN,
+              "iuacDrnFE8KjgjBautWojN71t": process.env.API_KEY_SECRET
+               }
+          })
+        .then(response=>{
+          //  console.log("res1",res)
+            return response.text();
+        })
+        .then(response=>{
+            console.log("res2",JSON.stringify(response));
+            res.status(200);
+            res.write(response);
+            return  res.end();
+        })
+        .catch(error=>{
+            console.log("Exception : ",error);
+        });
+        
+       }
+
+});
+
+module.exports = router;
--- routes/twitterAPI/getUsername.js
+++ routes/twitterAPI/getUsername.js
@@ -1,20 +1,58 @@
+require('dotenv').config();
+const fetch = require('node-fetch');
 var express = require('express');
 var router = express.Router();
 const got = require('got');
 
-router.use('/',function(res, req){
+router.use('/',function(req, res){
 
-   // let username = trim(req.body.username);
-   fetch("https://api.twitter.com/2/users/by/username/rickylart").then(
-    async (response) => {
-      const data = await response.json();
-      res.status(200).send(JSON.stringify(data.body));
+   var userID = req.body.username;
+
+   //checking if the userID is an array
+   if(Array.isArray(userID)){
+
+    //loop through the userIDs
+    // userID.forEach((element,index) => {
+    //     console.log(element);
+        
+    //    pickUserID = element;
+
+    //     return pickUserID;
+    //    });
+
+       console.log(userID);
+
+    fetch(`https://api.twitter.com/2/users/by?usernames=${userID}`,{
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization":process.env.BEARER_TOKEN,
+          "iuacDrnFE8KjgjBautWojN71t":process.env.API_KEY_SECRET
+           }
+      })
+    .then(response=>{
+      //  console.log("res1",res)
+        return response.text();
+    })
+    .then(response=>{
+        console.log("res2",JSON.stringify(response));
+        res.status(200);
+        res.write(response);
+        return  res.end();
+    })
+    .catch(error=>{
+        console.log("Exception : ",error);
     });
-
+   }
+   else{
+    res.status(400);
+    res.write(JSON.stringify("Please send a vaild array data"));
+    return  res.end();
+   }
 
     // (async () => {
     //     try {
-    //         var response = await got.get(`https://api.twitter.com/2/users/by/username/rickylart`,
+    //         var response = await got(`https://api.twitter.com/2/users/by/username/${username}`,
     //         {
     //             headers:{
     //                 "Authorization":"Bearer AAAAAAAAAAAAAAAAAAAAAJJUWAEAAAAAJ3buxe36tPsWjaFJWj9Gju37VpM%3DQoInR12MQNgWqWQMShINRbpE2WUEZ8yDG5Ow0DUdw3rbHRo9YO",
@@ -23,9 +61,15 @@ router.use('/',function(res, req){
     //         }
     //         );
     //          console.log(response.body);
+    //          res.status(200);
+    //          res.write(response.body);
+
+    //         return res.end();
     //     } catch (error) {
     //         console.log(error);
+    //          res.status(200).send(JSON.stringify(error));
     //         //=> 'Internal server error ...'
+    //         return res.end();
     //     }
     // })();
 
